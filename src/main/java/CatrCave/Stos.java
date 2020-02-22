e54c19612301c38712705a0fcbc748d00889c82a
Stah <stahthecat@gmail.com>
2020-02-22 11:01:34 +0100

    Stworzenie Repo

---
+++ src/main/java/CatrCave/Stos.java
@@ -0,0 +1,41 @@
+package CatrCave;
+
+public class Stos {
+
+    int[] stos;
+    int topElement;
+
+    public Stos(int size) {
+        this.stos = new int[size];
+        this.topElement = 0;
+    }
+
+    public void push(int x) {
+        stos[topElement] = x;
+        topElement++;
+    }
+
+    public int pop() {
+        int temp = stos[topElement - 1];
+        topElement--;
+        return temp;
+    }
+
+
+    public int size() {
+        return topElement;
+    }
+
+    public boolean isEmpty() {
+
+        if (topElement == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int[] getStos() {
+        return stos;
+    }
+}
